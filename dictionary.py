04b357ae887e26a9a0f646c083e5e53388821804
Ahmad12333 <ahmad.aldon@outlook.com>
2022-07-26 10:02:20 +0300

    adding_new_files

---
+++ dictionary.py
@@ -0,0 +1,25 @@
+monthConversations = {
+    "jan": "january",
+    "feb": "february",
+    "mar": "March",
+    "apr": "April",
+}
+#print(monthConversations.get("apr"))
+#print(monthConversations["jan"])
+############################ While Loops :#######################
+i =1 
+#while i <= 10:
+#    print(i)
+#   i +=1
+#print("Done with loop")  
+#print(i)
+# ################################### For LOOPS : ###################################
+for letter in "Giraffe Academy":
+    print(letter) 
+friends = ["Noor", "Aya", "Ghaleb"] 
+for friend in friends:
+    print(friend)
+for index in range(10):
+    print(index)
+for index in range(3,10):
+    print(index)
---
+++ main.py
@@ -0,0 +1,14 @@
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Ghaleb", "Noor", "Noor", "Ali", "Rand", "Aya"]
+#friends[2] = "Mike"
+#friends.extend(lucky_numbers)
+#friends.append("Mais")
+#friends.insert(1,"Hala")
+#friends.clear()
+#friends.pop()
+#friends.sort()
+#lucky_numbers.reverse()
+print(lucky_numbers)
+print(friends)
+##friends2 = friends.copy()
+#print(friends2)
